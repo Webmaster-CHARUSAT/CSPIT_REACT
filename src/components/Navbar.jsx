44d1fb414fc3004df222237dd280e0adeff13905
meetjaka <meetjaka46@gmail.com>
2026-09-25 19:15:34 +0530

    Update two faculty photo and newsletter

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const topLinks = [
   { to: "/events", label: "Enrichment Activities", external: true },
   { to: "#placement", label: "Placement", external: false },
   { to: "#gallery", label: "Gallery", external: false },
-  { to: "/news-letter-Jan-March-2026/", label: "Newsletter", external: true },
+  { to: "/news-letter-Apr-Jun-2026/", label: "Newsletter", external: true },
   {
     to: "https://www.charusat.ac.in/scholarship",
     label: "Scholarships",
